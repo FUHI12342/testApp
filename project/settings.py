110068f491799c523a942d3395828f7bdc65db8e
hirosan <17281756animebaka@gmail.com>
2024-01-31 22:03:00 +0900

    6th commit

--- project/settings.py
+++ project/settings.py
@@ -121,14 +121,12 @@ from dotenv import load_dotenv
 import os
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static_local',
-]
+#STATICFILES_DIRS = [ BASE_DIR /'static_local',]
 #BASE_DIR = Path(__file__).resolve().parent.parent.parent
-PARENT_DIR = BASE_DIR.parent
+#PARENT_DIR = BASE_DIR.parent
 #env_path = PARENT_DIR / "auth/.env"
 #load_dotenv(env_path)
-STATIC_ROOT = PARENT_DIR / 'site/public/static'
+#STATIC_ROOT = PARENT_DIR / "site/public/static"
 #SECRET_KEY = os.environ.get("secret_key")
 
 
