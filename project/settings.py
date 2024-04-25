07326d9599b0551e10bd886ebbf760b62ae73329
hirosan <17281756animebaka@gmail.com>
2024-04-25 11:53:27 +0900

    setting IPaddress&InstalledApp CHANGE

--- project/settings.py
+++ project/settings.py
@@ -22,10 +22,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&670@la%)g1zo2y7(+4+^pl00sb(cjl4rpvkf@2ly)eo+a$1k!'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# ローカル
+# DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# 本番
+DEBUG = False
+
+ALLOWED_HOSTS = ['104.248.154.35']
 
 
 # Application definition
@@ -39,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testApp',  # Add this line
+    #'testApp', 
     'rest_framework'
 ]
 
