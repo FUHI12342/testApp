7ec94da99220822d678d6c33d571db7398e888ec
hirosan <17281756animebaka@gmail.com>
2024-05-16 13:36:17 +0900

    240516 1st commit message

--- booking/views.py
+++ booking/views.py
@@ -285,16 +285,17 @@ from linebot.models import TextSendMessage
 from django.urls import reverse
 from urllib.parse import quote
 from linebot.exceptions import LineBotApiError
+from django.http import JsonResponse
+import json
 
 class PayingSuccessView(View):
     def post(self, request):
-
         # 決済サービスからのレスポンスを解析
-        payment_response = request.POST
+        payment_response = json.loads(request.body)
         print('payment_response' + str(payment_response))
         
         # 決済が成功したかどうかを確認
-        if payment_response.get('status') == 'paid':
+        if payment_response.get('type') == 'payment.succeeded':
             # 注文IDを取得
             order_id = payment_response.get('orderId')
 
@@ -355,7 +356,7 @@ class PayingSuccessView(View):
         print(message_text)
         
         # レスポンスを直接作成して返す
-        return HttpResponse('Payment successful and message sent.', content_type='text/plain')
+        return JsonResponse({'status': 'success'})
     
 class LineSuccessView(View):
     def get(self, request):
