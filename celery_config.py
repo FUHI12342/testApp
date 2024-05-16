b3259260884f4bfbce58884c2bbdb9d65ace6d4d
FUHI12342 <17281756animebaka@gmail.com>
2024-05-16 04:41:19 +0000

    Your commit message

--- celery_config.py
+++ celery_config.py
@@ -1,5 +1,15 @@
 from __future__ import absolute_import, unicode_literals
+import os
 from celery import Celery
+
+# Djangoの設定をロード
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+import django
+django.setup()  # Djangoの設定をロード
+
+from django.conf import settings  # Djangoの設定をインポート
+
 print('celery_config.py')
 app = Celery('booking')
 
@@ -13,6 +23,9 @@ app.conf.beat_schedule = {
         'schedule': 60.0,  # 60秒ごとに実行
     },
 }
+
+# booking.viewsを明示的にインポート
+import booking.views  # これが重要です
 
 print('Before autodiscover_tasks:', app)
 app.autodiscover_tasks()
